83beba453ac8e6453a012181847a31a9e2998814
Gigi <gomurashvili17@gmail.com>
2025-03-31 23:06:00 +0400

    added

---
+++ burger.js
@@ -0,0 +1,4 @@
+export function burger() {
+    console.log("hi");
+    
+}
--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-// js oop
+import { burger } from "./burger.js";
+burger();
 
 const Person = function (fname, bYear) {
   this.firstName = fname;
-  this.birthYear = bYear;
+  this.BirthYear = bYear;
 };
-//prototype - არის მექანიზმი,რომელთაც ჯანასკრიპტის ობიექტები ერთმანეთისგან თვისებებს მემკვიდრეობენ.
 Person.prototype.getAge = function () {
-  console.log(2024 - this.birthYear);
+  console.log(2024 - this.BirthYear);
 };
-const newObj1 = new Person("anna", 2004);
-const newObj2 = new Person("nini", 1990);
+const newObj = new Person("anna", 2004);
+console.log(newObj);
+newObj.getAge();
 
-console.log(newObj1);
-console.log(newObj2);
-
-newObj1.getAge();
-newObj2.getAge();
-
-// class oop
-//class expression
-// const PersonInfo = class {
-
-// }
-//class declaration
 class PersonInfoNew {
-  constructor(firstName2, birthYear2) {
-    this.userFirstName = firstName2;
-    this.userBirthYear = birthYear2;
+  constructor(fisrtName, birthYear) {
+    this.userfirstname = fisrtName;
+    this.userbirthYear = birthYear;
   }
   printAge() {
-    console.log(2024 - this.userBirthYear);
+    console.log(2024 - this.userbirthYear);
   }
 }
-const infoObj3 = new PersonInfoNew("luka", 2005);
-console.log(infoObj3);
 
-infoObj3.printAge();
+const infoobj = new PersonInfoNew("luka", 2005);
+console.log(infoobj);
+infoobj.printAge();
+
+axios
+  .get("http://localhost:3000/posts")
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
